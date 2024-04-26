bedbfacf5e9ea5e9da6dbc052cd53a4940170f23
Hassan Ali <hassan@turingtechnologies.org>
2024-04-26 17:04:06 +0500

    community and collection page added

---
+++ src/lib/actions/question.action copy.ts
@@ -0,0 +1,161 @@
+"use server";
+
+import Question from "@/database/question.modal";
+import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.modal";
+import User from "@/database/user.modal";
+import { CreateQuestionParams, QuestionVoteParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+
+export async function createQuestion(params: CreateQuestionParams) {
+  console.log("create question start @@@2");
+  // eslint-disable-next-line no-useless-catch
+  try {
+    connectToDatabase();
+    console.log("params received @@@ ", params);
+    const { title, content, tags, author, path } = params;
+    // Create the question
+    console.log("tags --->", tags);
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+    console.log("tags -->", tags);
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag);
+    }
+    console.log("tagDocuments --->", tagDocuments);
+    await Question.findByIdAndUpdate(question._id, { tags: tagDocuments });
+
+    revalidatePath(path);
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function getQuestionById(id: string) {
+  // eslint-disable-next-line no-useless-catch
+
+  try {
+    if (id) {
+      connectToDatabase();
+      return await Question.findById(id)
+        .populate("tags", { modal: Tag })
+        .populate("author", { modal: User });
+    }
+  } catch (e) {
+    console.log("error in getQuestionById", e);
+    throw e;
+  }
+}
+
+export async function getQuestions() {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    connectToDatabase();
+    return await Question.find()
+      .populate("tags", { modal: Tag })
+      .populate("author", { modal: User })
+      .sort({ createdAt: -1 });
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function upvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasUpvoted, hasDownvoted, path } = params;
+
+    let updateQuery = {};
+
+    if (hasUpvoted) {
+      updateQuery = { $pull: { upVotes: userId } };
+    } else if (hasDownvoted) {
+      updateQuery = {
+        $pull: { downVotes: userId },
+        $push: { upVotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { upVotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+
+    if (!question) {
+      throw new Error(`❌ Question not found ❌`);
+    }
+
+    // Increment author's reputation by +1/-1 for
+    // upvoting/revoking an upvote to the question
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasUpvoted ? -1 : 1 },
+    });
+
+    // Increment author's reputation by +10/-10 for
+    // recieving an upvote/downvote to the question
+    await User.findByIdAndUpdate(question.author, {
+      $inc: { reputation: hasUpvoted ? -10 : 10 },
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
+    throw error;
+  }
+}
+
+export async function downvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasUpvoted, hasDownvoted, path } = params;
+
+    let updateQuery = {};
+
+    if (hasDownvoted) {
+      updateQuery = { $pull: { downvoteQuestion: userId } };
+    } else if (hasUpvoted) {
+      updateQuery = {
+        $pull: { upVotes: userId },
+        $push: { downVotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { downVotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+
+    if (!question) {
+      throw new Error(`❌ Question not found ❌`);
+    }
+
+    // Decrement author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasDownvoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(question.author, {
+      $inc: { reputation: hasDownvoted ? -10 : 10 },
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
+    throw error;
+  }
+}
